8aef4999d40fa0cedd3deb56daadbcd09eeece09
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2016-09-05 15:24:06 +0200

    js: Implement startScan(), stopScan() and didDiscoverDevice()

--- index.js
+++ index.js
@@ -16,6 +16,42 @@ const didChangeState = (callback) => {
   };
 };
 
+const DefaultScanOptions = {
+  uuids: [],
+  timeout: 10000,
+};
+
+const Scan = {
+  stopAfter: (timeout) => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        ReactNativeBluetooth.stopScan()
+          .then(resolve)
+          .catch(console.log.bind(console));
+      }, timeout);
+    })
+  }
+}
+
+const startScan = (customOptions = {}) => {
+  let options = Object.assign({}, DefaultScanOptions, customOptions);
+
+  return ReactNativeBluetooth.startScan(options.uuids).then(() => Scan);
+}
+
+const didDiscoverDevice = (callback) => {
+  var subscription = NativeAppEventEmitter.addListener(
+    ReactNativeBluetooth.DeviceDiscovered,
+    callback
+  );
+
+  return function() {
+    subscription.remove();
+  };
+};
+
 export default {
   didChangeState,
+  startScan,
+  didDiscoverDevice,
 };
