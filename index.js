b992a521744676d9f38e1bb7ac4288cc42e0f219
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2016-09-05 10:18:20 +0200

    js: Introduce Bluetooth.didChangeState

--- index.js
+++ index.js
@@ -3,5 +3,19 @@
 import { NativeAppEventEmitter, NativeModules } from 'react-native';
 const ReactNativeBluetooth = NativeModules.ReactNativeBluetooth;
 
+const didChangeState = (callback) => {
+  var subscription = NativeAppEventEmitter.addListener(
+    ReactNativeBluetooth.StateChanged,
+    callback
+  );
+
+  ReactNativeBluetooth.notifyCurrentState();
+
+  return function() {
+    subscription.remove();
+  };
+};
+
 export default {
+  didChangeState,
 };
