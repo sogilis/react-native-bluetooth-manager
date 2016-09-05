c4c78f5580cd7cbb2991e43c638389d6e4d19e00
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2016-09-05 16:23:38 +0200

    android: Implement startScan(), stopScan() and DeviceDiscovered

--- android/src/main/java/com/sogilis/ReactNativeBluetooth/ReactNativeBluetoothModule.java
+++ android/src/main/java/com/sogilis/ReactNativeBluetooth/ReactNativeBluetoothModule.java
@@ -1,18 +1,26 @@
 package com.sogilis.ReactNativeBluetooth;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.util.Log;
 
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class ReactNativeBluetoothModule extends ReactContextBaseJavaModule {
 
@@ -23,9 +31,14 @@ public class ReactNativeBluetoothModule extends ReactContextBaseJavaModule {
                 new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
     }
 
+    // Logging
+    private static final String TAG = ReactNativeBluetoothModule.class.getSimpleName();
+
     // Events
     private static final String EVENT_STATE_CHANGED =
             ReactNativeBluetoothModule.class.getCanonicalName() + ".EVENT_STATE_CHANGED";
+    private static final String EVENT_DEVICE_DISCOVERED =
+            ReactNativeBluetoothModule.class.getCanonicalName() + ".EVENT_DEVICE_DISCOVERED";
 
     // States
     private static final String STATE_ENABLED = "enabled";
@@ -64,10 +77,71 @@ public class ReactNativeBluetoothModule extends ReactContextBaseJavaModule {
         }
     };
 
+    @ReactMethod
+    public void startScan(ReadableArray uuidStrings, Promise promise) {
+        final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null) {
+            promise.reject("Bluetooth not supported");
+            return;
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            promise.reject("Bluetooth disabled");
+            return;
+        }
+
+        UUID[] uuids = new UUID[uuidStrings.size()];
+        for(int i=0; i<uuidStrings.size(); i++) {
+            uuids[i] = UUID.fromString(uuidStrings.getString(i));
+        }
+
+        bluetoothAdapter.startLeScan(uuids, scanCallback);
+        promise.resolve(null);
+    }
+
+    private BluetoothAdapter.LeScanCallback scanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            Log.d(TAG, "Device discovered: \"" + device.getName() + "\" @" + device.getAddress());
+            notifyDeviceDiscovered(device);
+        }
+    };
+
+    private void notifyDeviceDiscovered(BluetoothDevice device) {
+        WritableMap deviceMap = new WritableNativeMap();
+
+        deviceMap.putString("address", device.getAddress());
+        deviceMap.putString("name", device.getName());
+
+        getReactApplicationContext().
+                getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).
+                emit(EVENT_DEVICE_DISCOVERED, deviceMap);
+    }
+
+    @ReactMethod
+    public void stopScan(Promise promise) {
+        final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null) {
+            promise.reject("Bluetooth not supported");
+            return;
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            promise.reject("Bluetooth disabled");
+            return;
+        }
+
+        bluetoothAdapter.stopLeScan(this.scanCallback);
+        promise.resolve(null);
+    }
+
     @Override public Map<String, Object> getConstants() {
         final Map<String, Object> constants = new HashMap<>();
 
         constants.put("StateChanged", EVENT_STATE_CHANGED);
+        constants.put("DeviceDiscovered", EVENT_DEVICE_DISCOVERED);
 
         return constants;
     }
