4c8cff1ebb261540daedfffce91cebe8bb68450a
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2016-09-13 13:40:26 +0200

    android: Remove useless imports

--- android/src/main/java/com/sogilis/ReactNativeBluetooth/events/EventEmitter.java
+++ android/src/main/java/com/sogilis/ReactNativeBluetooth/events/EventEmitter.java
@@ -1,16 +1,10 @@
 package com.sogilis.ReactNativeBluetooth.events;
 
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattService;
-import android.util.Base64;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+
 import static com.sogilis.ReactNativeBluetooth.Constants.MODULE_NAME;
 
 public class EventEmitter {
